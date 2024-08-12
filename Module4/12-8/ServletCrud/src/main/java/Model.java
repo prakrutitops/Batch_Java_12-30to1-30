d0096783c2658e755dc4ba699f2e7828a0c675a9
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2024-08-12 15:15:20 +0530

    commit

---
+++ Module4/12-8/ServletCrud/src/main/java/Model.java
@@ -0,0 +1,39 @@
+
+public class Model 
+{
+	int id;
+	String fname,lname,email,password;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+}
---
+++ Module4/12-8/ServletCrud/src/main/java/ProductInsertServlet.java
@@ -0,0 +1,50 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/pinsert")
+public class ProductInsertServlet extends HttpServlet
+{
+	//doget
+	//dopost
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException 
+	{
+		
+		resp.setContentType("text/html");
+		PrintWriter out = resp.getWriter();//when you want to print anything on webpage
+		
+		String pname = req.getParameter("pname");
+		String pprice = req.getParameter("pprice");
+		String pdes = req.getParameter("pdes");
+	
+	
+		//out.print(fn+" "+ln+" "+em+" "+ps);
+	
+		//data set in Model
+		ProductModel m = new ProductModel();
+		m.setPname(pname);
+		m.setPprice(pprice);
+		m.setPdes(pdes);
+		
+		int status = Dao.pinsert(m);
+		
+		if(status>0)
+		{
+			out.print("success");
+		}
+		else
+		{
+			out.print("fail");
+		}
+		
+		
+	}
+	
+}
