a4e6955d91cbf58b11bd0e0019470d849a51fbc0
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2024-11-11 15:16:19 +0530

    commit

---
+++ Module5/springbootdemo-1/src/main/java/com/example/demo/Springbootdemo1Application.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootdemo1Application {
+
+	public static void main(String[] args) 
+	{
+		SpringApplication.run(Springbootdemo1Application.class, args);
+	}
+
+}
---
+++ Module5/springbootdemo-1/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.model.User;
+
+
+@Controller
+public class UserController 
+{
+	@RequestMapping("/")
+	public String index(Model model)
+	{
+		return "index";
+	}
+}
